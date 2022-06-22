cb8dbe5d96062f4a86d23ea28ecdcb67031cd2e1
Parth Arora <aror0055@algonquinlive.com>
2022-06-21 23:24:19 -0400

    BizLand

---
+++ about.php
@@ -0,0 +1,57 @@
+<?php
+include 'header.php';
+include 'sections.php';
+?>
+
+<div id="container">
+  <!-- ======= About Section ======= -->
+  <section id="about" class="about section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>About</h2>
+          <h3>Find Out More <span>About Us</span></h3>
+          <p>Ut possimus qui ut temporibus culpa velit eveniet modi omnis est adipisci expedita at voluptas atque vitae autem.</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6" data-aos="fade-right" data-aos-delay="100">
+            <img src="assets/img/about.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 content d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="100">
+            <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
+            <p class="fst-italic">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+              magna aliqua.
+            </p>
+            <ul>
+              <?php
+            foreach($sections as $section) { ?>
+              <li>
+                <i class="<?php echo $section["class"]; ?>"></i>
+                <div>
+                  <h5><?php echo $section["title"]; ?></h5>
+                  <p><?php echo $section["body"]; ?></p>
+                </div>
+              </li>
+              <?php } ?> 
+              
+            </ul>
+            <p>
+              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+</div>
+
+
+<?php
+  require 'footer.php'
+  ?>
+
+  
---
+++ sections.php
@@ -0,0 +1,22 @@
+<?php 
+$sections = [
+    [
+      "id" => 0,
+      "class" => "bx bx-store-alt",
+      "title" => "Ullamco laboris nisi ut aliquip consequat",
+      "body" => "Magni facilis facilis repellendus cum excepturi quaerat praesentium libre trade"
+    ],
+    [
+      "id" =>1,
+      "class" => "bx bx-cart",
+      "title" => "Magnam soluta odio exercitationem reprehenderi",
+      "body" => "Quo totam dolorum at pariatur aut distinctio dolorum laudantium illo direna pasata redi"
+    ],
+     [
+      "id" => 2,
+      "class" => "bx bx-bar-chart-alt-2",
+      "title" => "Etiam varius fermentum facilisis",
+      "body" => "Integer feugiat tempus facilisis. Integer eleifend lacinia ante et vehicula. Nunc vel dolor a sapien sodales pulvinar."
+    ]
+     ];
+?>
